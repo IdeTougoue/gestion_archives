995bd51466460b3087b91c907ce04e823765e3fb
ide <idekti38@gmail.com>
2021-03-13 16:22:49 +0100

    integration du template de base (AdminLTE 3)

---
+++ pages/GestionDocuments/ListeDocuments.php
@@ -0,0 +1,18 @@
+<?php
+include('../_Includes/Header.php');
+?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        
+
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+<?php
+include('../_Includes/footer.php');
+?>
---
+++ pages/_Includes/footer.php
@@ -0,0 +1,21 @@
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.1.0-rc
+    </div>
+    <strong>Copyright &copy; 2021 <a href="#">KAKO IDE</a>.</strong> All rights reserved.
+  </footer>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../../Libraries/Js/jquery.min.js"></script>
+<!-- -->
+<script src="../../Libraries/Js/all.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../Libraries/Js/bootstrap.bundle.js"></script>
+<!-- AdminLTE App -->
+<script src="../../Libraries/Js/adminlte.js"></script>
+</body>
+</html>
