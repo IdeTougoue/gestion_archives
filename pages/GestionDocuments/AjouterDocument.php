dfa5640c062b9ff7e8c8b82093ef35447ad71563
ide <idekti38@gmail.com>
2021-03-28 18:58:58 +0100

    disign des pages de Gestion des documents

--- pages/GestionDocuments/AjouterDocument.php
+++ pages/GestionDocuments/AjouterDocument.php
@@ -1,13 +1,64 @@
 <?= include ( '../_Includes/Header.php' ); ?>
-
+ 
   <!-- Emballage de contenu. Contient le contenu de la page -->
   <div  class = " content-wrapper ">
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>AJOUTER UN DOCUMENT</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
     <!-- En-tête de contenu (En-tête de page) -->
     <section class="content-header">
       <div class="container-fluid">
+      <div class="col-md-12">
+      <div class="card card-primary">
+          <div class="card-header">
+            <h3 class="card-title">Formulaire d'ajout</h3>
+          </div>
+          <!-- /.card-header -->
+          <!-- form start -->
+          <form>
+            <div class="card-body">
+              <div class="form-group col-md-4">
+                <label for="exampleInputEmail1">Nom du document</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <div class="input-group">
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="exampleInputFile">
+                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                  </div>
+                  <div class="input-group-append">
+                    <span class="input-group-text">Upload</span>
+                  </div>
+                </div>
+              </div>
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+            </div>
+            <!-- /.card-body -->
 
+            <div class="card-footer">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>
+        </div>
+        <!-- /.card -->
 
-
+          </div>
+          <!-- /.col -->
       </div> 
       <!-- /.conteneur-fluide -->
     </section>
